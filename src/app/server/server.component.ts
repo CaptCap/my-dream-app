d94e5cb602ec9efa396e7a0d379e352759e0c09b
CaptCap <112699343+CaptCap@users.noreply.github.com>
2023-02-14 14:32:37 +0200

    mydream

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+
+  // selector: '[app-server]',
+  // selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  serverId = 19;
+  serverStatus = 'offline';
+   getServerStatus() {
+    return this.serverStatus;
+   }
+constructor() {
+
+}
+ngOnInit() : void {
+
+}
+}
